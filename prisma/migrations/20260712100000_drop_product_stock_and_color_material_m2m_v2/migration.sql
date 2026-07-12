08c6fd1740a79dcfe050d1cacbcf9cd0c2372477
Artem Honcharenko <artemgonchar7@gmail.com>
2026-07-12 15:41:50 +0200

    chore: seeded some real images

---
+++ prisma/migrations/20260712100000_drop_product_stock_and_color_material_m2m_v2/migration.sql
@@ -0,0 +1,20 @@
+-- DropForeignKey
+ALTER TABLE "_ColorToProduct" DROP CONSTRAINT "_ColorToProduct_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ColorToProduct" DROP CONSTRAINT "_ColorToProduct_B_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_MaterialToProduct" DROP CONSTRAINT "_MaterialToProduct_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_MaterialToProduct" DROP CONSTRAINT "_MaterialToProduct_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "stock";
+
+-- DropTable
+DROP TABLE "_ColorToProduct";
+
+-- DropTable
+DROP TABLE "_MaterialToProduct";
